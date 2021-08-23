e8bf7210c225a8a79ea308b6ed831a75c31a0eed
lnalmeida <l.n.almeida.ti@gmail.com>
2021-08-22 21:54:37 -0300

    Configure and connect on Mongo Atlas

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 
+import mongoose from 'mongoose';
+
 class App {
 public app: express.Application;
 
 public constructor() {
   this.app = express();
   this.app.use(cors());
+
+  this.initMongoose();
+  this.connectDatabase();
+}
+
+public initMongoose():void {
+  mongoose.set('runValidators', true);
+}
+
+public connectDatabase():void {
+  mongoose.connect('mongodb+srv://sc-admin:gsJ24BNpSUat6lqN@study-cluster.svi6a.mongodb.net/curso-full-stack?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  });
+  console.log('Mongoose connected');
 }
 
 public listen(port: number): void {
